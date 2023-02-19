d949ed1d3900f372a2ce3eb6a06722c8c3026172
Faith Hidalgo <imfaithangelou@gmail.com>
2023-02-20 04:29:56 +0800

    commit Quantum app

---
+++ resources/views/tradingtopics.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="page-outer trending-outer">
+                <div class="page-inner trending-inner">
+
+                  <div class="page-head-n-sort">
+                    <div class="head-left-wrap">
+                      <span class="profile-heading">
+                        Trending</span>
+                    </div>  <!-- END .head-left-wrap -->
+                  </div>  <!-- END .page-head-n-sort -->
+
+
+                  <div class="trending-tool-outer">
+                    <div class="trending-tool-inner">
+
+                      <span class="trending-tag">
+                        marketing
+                      </span>
+
+                                    <span class="trending-tag">
+                                      business
+                                    </span>
+
+                                    <span class="trending-tag">
+                                      eggo waffles
+                                    </span>
+
+                                    <span class="trending-tag">
+                                      syrup
+                                    </span>
+
+                                    <span class="trending-tag">
+                                      cars with muscles
+                                    </span>
+
+                                    <span class="trending-tag">
+                                      squirrel suit madness
+                                    </span>
+
+                                    <span class="trending-tag">
+                                      wimbly jimbly
+                                    </span>
+
+                                    <span class="trending-tag">
+                                      frodo baggins
+                                    </span>
+
+                                    <span class="trending-tag">
+                                      the shire
+                                    </span>
+
+                                    <span class="trending-tag">
+                                      MY PRECIOUS!
+                                    </span>
+
+
+                    </div>  <!-- END .trending-tool-inner -->
+                  </div>  <!-- END .trending-tool-outer -->
+
+                </div>  <!-- END .trending-inner -->
+              </div>  <!-- END .trending-outer -->
+@endsection
