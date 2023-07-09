8dc7a2934d7111363527792cb47e30c61966fb2b
Faith Hidalgo <imfaithangelou@gmail.com>
2023-07-09 21:20:14 +0800

    : fixes edit post, login , register,  forgot pass

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,12 +11,33 @@
         </div>
 
         <form method="POST" action="{{ route('register') }}">
-            @csrf
+            @csrf           
+                        
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+
+            @if (session()->has('errors'))
+    <div class="alert alert-danger">
+        <ul>
+            @foreach (session('errors')->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>  
+    </div>
+@endif
+        
            
             <div class="row mb-3">
 
                 <div class="col-md-12 m1em" >
-                    <input id="firstname" placeholder="First name" type="text" class="form-control @error('firstname') is-invalid @enderror" name="firstname" value="{{ old('name') }}" required autocomplete="name">
+                    <input id="firstname" placeholder="First name" type="text" class="form-control @error('firstname') is-invalid @enderror" name="firstname" value="{{ old('firstname') }}" autocomplete="name">
 
                     @error('name')
                         <span class="invalid-feedback" role="alert">
@@ -29,7 +50,7 @@
             <div class="row mb-3">
 
                 <div class="col-md-12 m1em" >
-                    <input id="lastname" placeholder="Last name" type="text" class="form-control @error('lastname') is-invalid @enderror" name="lastname" value="{{ old('name') }}" required autocomplete="name">
+                    <input id="lastname" placeholder="Last name" type="text" class="form-control @error('lastname') is-invalid @enderror" name="lastname" value="{{ old('lastname') }}" autocomplete="name">
 
                     @error('name')
                         <span class="invalid-feedback" role="alert">
@@ -42,7 +63,7 @@
             <div class="row mb-3">
 
                 <div class="col-md-12 m1em">
-                    <input id="email" placeholder="Email Address" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                    <input id="email" placeholder="Email Address" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email">
 
                     @error('email')
                         <span class="invalid-feedback" role="alert">
