b68091fa55f0c5f30f283f9bb81948adffbcc9cb
Faith Hidalgo <imfaithangelou@gmail.com>
2024-02-19 05:44:41 +0800

    * sync files from FTP

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\Auth\RegisterController;
-
+use App\Models\User;
+use Illuminate\Support\Facades\DB; 
 
 /*
 |--------------------------------------------------------------------------
@@ -16,11 +17,38 @@ use App\Http\Controllers\Api\V1\Auth\RegisterController;
 |
 */
 
+
+Route::get('wp', function () {
+	$r = $_REQUEST;
+	
+	if(isset( $r['wp_user_id'] ) ) {
+		$user = User::create([
+		'email' => $r['wp_email'],
+		'password' => Hash::make($r['wp_password']),
+		]);
+			
+		if( $user->id ) {
+			DB::table('app_usermeta')->insert([
+				['user_id' => $user->id, 'meta_key' => 'wp_user_id', 'meta_value' => $r['wp_user_id']],
+				['user_id' => $user->id, 'meta_key' => 'id_subscription', 'meta_value' => $r['wp_subscription']],
+				['user_id' => $user->id, 'meta_key' => 'wp_subscription', 'meta_value' => $r['name_subscription']],
+			]);
+			
+			return response()->json(['status' =>'success', 'laravel_id' => $user->id]);
+			
+		} else {
+			return response()->json(['status' =>'error']);
+		}
+		
+	}
+	
+	return response()->json(['status' =>'not available']);
+	
+	
+});
+
 Route::post('auth/register', RegisterController::class); // Define route using RegisterController method
 // Route::post('auth/register', RegisterController::class)->middleware('auth');
 
 Route::get('scrape/', [RegisterController::class, 'scrapeMetaTags']);
 
-Route::get('test', function () {
-    return response()->json(['message' => 'API route is working!']);
-});
