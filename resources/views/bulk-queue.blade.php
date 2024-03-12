5bc1b68a9803c1ed0a7a2cdfeaa23e35f62344ad
Faith <imfaithangelou@gmail.com>
2024-03-13 05:49:45 +0800

    : fixes on bulk and bulk-queue modal trigger

--- resources/views/bulk-queue.blade.php
+++ resources/views/bulk-queue.blade.php
@@ -7,72 +7,18 @@
     <div class="page-head-n-sort">
       <div class="head-left-wrap">
         <span class="profile-heading">
-          Bulk Queue</span>
-        {{-- <div class="toggle-wrapper">
-          <label class="toggleSwitch large">            
-            <input type="checkbox" id="post-status" {{ $toggle > 0 ? 'checked' : '' }}/>
-            
-            <span>
-                <span>INACTIVE</span>
-                <span>ACTIVE</span>
-            </span>
-            <a></a>
-          </label>
-        </div>   --}}
+          Bulk Queue
+        </span>        
       </div>  <!-- END .head-left-wrap -->
-      {{-- <div class="head-right-wrap">
-        <img src="{{ asset('public/')}}/ui-images/icons/add-post.svg" class="ui-icon new-promo-tweet" />
-        <img src="{{ asset('public/')}}/ui-images/icons/add-post.svg" class="ui-icon new-queue-cmdmod" />
-        <img src="{{ asset('public/')}}/ui-images/icons/pg-controls.svg" class="ui-icon filter-controls-toggle" />
-      </div>  --}}
+      
     </div>  <!-- END .page-head-n-sort -->
-
-    {{-- <div class="filter-controls">
-      <div class="drop-button-wrap filter-wrap queue-source-wrap">
-        <span class="white-select-button profile-filter-select">
-          <img src="{{ asset('public/')}}/ui-images/icons/17-promos.svg" class="ui-icon" />
-          Sort by Source:
-          <img src="{{ asset('public/')}}/ui-images/icons/pg-arrow.svg" class="ui-icon drop-arrow" />
-        </span>
-        <ul class="page-filters-dropdown profile-filter-dropdown frosted queue-page-dd">
-          <li id="all">
-            <img src="{{ asset('public/')}}/ui-images/icons/04-queue.svg" class="ui-icon" />
-            All (0)
-          </li>
-          <li id="non-campaign">
-            <img src="{{ asset('public/')}}/ui-images/icons/pg-twitter.svg" class="ui-icon" />
-            Non-Campaign (0)
-          </li>           
-          <li id="retweet">
-            <img src="{{ asset('public/')}}/ui-images/icons/pg-retweet.svg" class="ui-icon" />
-            Retweets
-          </li>
-        </ul>
-      </div>  <!-- END .filter-wrap -->
-      <div class="drop-button-wrap sort-wrap">
-        <span class="white-select-button profile-sort-select">
-          <img src="{{ asset('public/')}}/ui-images/icons/pg-sort.svg" class="ui-icon" />
-          Month:
-          <img src="{{ asset('public/')}}/ui-images/icons/pg-arrow.svg" class="ui-icon drop-arrow" />
-        </span>
-        <ul class="page-filters-dropdown profile-sort-dropdown queue-months-dropdown frosted">       
-          <li>
-            <img src="{{ asset('public/') }}/ui-images/icons/07-schedule.svg" class="ui-icon" />All
-          </li>                     
-        </ul>
-      </div>  
-    </div>   --}}
+   
 
     <div id="spinner" style="display: none;">
       Loading
     </div>
     <div id="error" style="display: none;">      
-    </div>
-    {{-- @if(session()->has('message'))
-        <div class="alert alert-success">
-            {{ session('message') }}
-        </div>
-    @endif --}}
+    
     <div class="queued-posts-outer" id="queuePage" >
       <div class="queued-posts-inner">
         <div class="queue-day-wrapper page-wrapper">                                
@@ -83,7 +29,6 @@
 
   </div>  <!-- END .profile-inner -->
 </div>  <!-- END .profile-outer -->
-{{-- <button id="openModalButton" style="display: none;" data-toggle="modal" data-target="#editModal"></button> --}}
 
 <style>
   #container {
@@ -98,7 +43,4 @@
   background-color: #f0f0f0;
 }
 </style>
-@endsection
-@section('scripts')
-<script type='text/javascript' src="{{asset('public/js/posting.js')}}"></script>
 @endsection
