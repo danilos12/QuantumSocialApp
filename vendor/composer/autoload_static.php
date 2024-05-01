501db1204cb2caf3b48af629b80d916b19ce636e
Andre Dan Dayaganon <andredandayaganon@gmail.com>
2024-05-01 14:53:56 +0800

    fix

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -325,8 +325,8 @@ class ComposerStaticInitcf14086919a839fe740e00554e4baff6
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
