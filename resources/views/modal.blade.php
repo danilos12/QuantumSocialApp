7bf7c960afd77275241dba55a9783f64f038eb4a
Faith Hidalgo <imfaithangelou@gmail.com>
2024-05-23 23:05:04 +0800

    * file clean up

--- resources/views/modal.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="modal-large-anchor faith-test">
-    <div class="modal-large-backdrop">
-        
-        <div class="content-modal"></div>
-
-    </div>  <!-- END .main-settings-background -->
-</div>  <!-- END .main-settings-anchor -->
