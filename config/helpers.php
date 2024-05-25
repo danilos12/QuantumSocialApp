7fdf015ce515f293ba0a25e1590368aeda389a2c
Faith Hidalgo <imfaithangelou@gmail.com>
2024-05-26 05:39:49 +0800

    : revert again

--- config/helpers.php
+++
@@ -1,7 +0,0 @@
-<?php
-    return [
-        'account_inactive_msg' => [
-            'message' => 'Your account is inactive. Please update your payment to continue using the features.',         
-        ]      
-    ]
-?>
--- test_script.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Cron job is working" >> /tmp/cron_test.log
-
